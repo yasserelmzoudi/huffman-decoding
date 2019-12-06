5616011bd00dd4748e2b22c55fa1b259e6b09c98
Yasser Elmzoudi <ye9@duke.edu>
2019-12-06 18:28:39 -0500

    compress

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -1,3 +1,4 @@
+import java.util.PriorityQueue;
 
 /**
  * Although this class has a history of several years,
@@ -40,15 +41,80 @@ public class HuffProcessor {
 	 * @param out
 	 *            Buffered bit stream writing to the output file.
 	 */
-	public void compress(BitInputStream in, BitOutputStream out){
-
-		while (true){
-			int val = in.readBits(BITS_PER_WORD);
-			if (val == -1) break;
-			out.writeBits(BITS_PER_WORD, val);
-		}
+	public void compress(BitInputStream in, BitOutputStream out)
+	{
+		int [] counts = readForCounts(in);
+		HuffNode root = makeTreeFromCounts(counts);
+		String[] codings = makeCodingsFromTree(root);
+		
+		out.writeBits(BITS_PER_INT, HUFF_TREE);
+		writeHeader(root, out);
+		
+		in.reset();
+		writeCompressedBits(codings, in, out);
 		out.close();
 	}
+	private int[] readForCounts(BitInputStream in)
+	{
+		int [] freq = new int[ALPH_SIZE + 1];
+
+		while (true)
+		{
+			int bits = in.readBits(BITS_PER_WORD);
+
+			if (bits == -1)
+			{
+				break;
+			}
+
+			freq[bits]++;
+		}
+
+		freq[PSEUDO_EOF] = 1;
+		in.reset();
+		return freq;
+	}
+
+	private HuffNode makeTreeFromCounts(int[] counts)
+	{
+		PriorityQueue<HuffNode> pq = new PriorityQueue<>();
+
+
+		for (int i = 0; i < counts.length; i++)
+		{
+			if (counts[i] > 0)
+			{
+				pq.add(new HuffNode(i, counts[i], null, null));
+			}
+		}
+
+		while (pq.size() > 1)
+		{
+			HuffNode left = pq.remove();
+			HuffNode right = pq.remove();
+			HuffNode t = new HuffNode(left.myValue,left.myWeight + right.myWeight, left, right);
+			pq.add(t);
+		}
+
+		HuffNode root = pq.remove();
+		return root;
+	}
+
+	private String[] makeCodingsFromTree(HuffNode root) {
+	}
+
+	private void writeHeader(HuffNode root, BitOutputStream out) {
+	}
+
+	private void writeCompressedBits(String[] codings, BitInputStream in, BitOutputStream out) {
+	}
+
+
+
+
+
+
+
 	/**
 	 * Decompresses a file. Output file must be identical bit-by-bit to the
 	 * original.
